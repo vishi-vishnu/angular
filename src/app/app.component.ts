b06a2628159eab88c66c933d1710cb8e01b58742
vishi-visihnu <vish08vishnu@gmail.com>
2025-02-09 10:01:50 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,35 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'VISHNU';
   college = 'KLN College of Engineering!';
   imageUrl =
-  'https://media.architecturaldigest.com/photos/5bdc8f9654719d268fefc80b/master/w_320%2Cc_limit/16%2520Vassar%2520Wiki.jpg';
-  imageUrl2 = 'https://www.shutterstock.com/image-photo/group-students-master-tablet-laptop-600nw-2347371743.jpg';
+    'https://www.shutterstock.com/image-photo/group-students-master-tablet-laptop-600nw-2347371743.jpg';
+  imageUrl2 =
+    'https://www.shutterstock.com/image-photo/group-students-master-tablet-laptop-600nw-2347371743.jpg';
   department = '';
   isDisabled = false;
-  textColor ='blue';
+  textColor = 'blue';
   isChecked = false;
   isError = true;
-  dynamicClass = 'highlight'; 
+  dynamicClass = 'highlight';
   isHighlighted = false;
 
-  message = "Are You Studying ECE Department?";
+  message = 'Are You Studying ECE Department?';
 
   username = '';
 
-  changeMessage(){
-    this.message = "OKAY Your studying in ECE dept."
+  changeMessage() {
+    this.message = 'OKAY Your studying in ECE dept.';
   }
-  changeMessage2(){
-    this.message = "NOW Your Not studying in ECE dept."
+  changeMessage2() {
+    this.message = 'NOW Your Not studying in ECE dept.';
   }
 
   updateUsername(event: Event) {
     this.username = (event.target as HTMLInputElement).value;
   }
-
 }
