f698d6b1c531cfb48ce325ac2bdb1a681ff512fd
Your Name <you@example.com>
2025-02-15 01:13:06 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
   styleUrl: './lifecycle.component.css',
   standalone:true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,FormsModule],
 })
-export class LifecycleComponent {
 
+export class LifecycleComponent implements DoCheck,OnInit{
+  message = '';
+
+  title = 'hello';
+  prevTitle = 'hello';
+
+  ngOnInit(): void {
+    this.message = 'Welcome to KLN colleg of engineering';
+  }
+
+
+  ngDoCheck(): void {
+    if (this.title !== this.prevTitle){
+      console.log("Your title has been changed",this.title );
+      this.prevTitle = this.title;
+    }
+  }
 }
+
+
